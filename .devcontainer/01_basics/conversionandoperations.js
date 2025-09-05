d570572f6d23d150ed67daa4a9f72ec73ab47853
Anany200326 <shreyg238@gmail.com>
2025-09-05 13:27:20 +0000

    conversion of data types

---
+++ .devcontainer/01_basics/conversionandoperations.js
@@ -0,0 +1,23 @@
+let score="55abc"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+//"33"=33
+//"44abc"=Nan (Not a Number) but typeof is of Number
+//true=1
+//false=0
+//" "=false /* Conversion into boolean 
+//"abcd"=true               */
+let isLoggedIn=1
+let booleanisloggedIn=Boolean(isLoggedIn)
+console.log(booleanisloggedIn)
+// similarly conversion to string 
+let someNumber=93
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
