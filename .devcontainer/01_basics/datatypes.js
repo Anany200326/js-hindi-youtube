a9733109900102ff4ef163d97b723be1a393da90
Anany200326 <shreyg238@gmail.com>
2025-09-05 12:25:37 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict";// treat all js code as newer version of js
+//alert(3+4)//used to generate popups in case of writing the code in console inside the web browser but it works differently in case of node js 
+//number
+// bigint
+//string
+//boolean
+//null = standalone value(representation of empty value)
+// undefined
+//symbol
+
+//objects
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 144533
+let accountEmail ="anany334@fie.com"
+var accountPassword="12345"
+accountCity="Kanpur"
+//accountId=2 not allowed
+accountEmail="hc@hc.com"
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity])
+let accountState;
+console.log(accountState);
